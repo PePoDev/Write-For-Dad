86fa6d1db1fcb60f2bc75ac79c30673e2bb73ff6
PePo <gmgame.1525@gmail.com>
2019-03-30 19:56:05 +0700

    Add dictionary serialize plugin

---
+++ Assets/Levels/Level 2/Scripts/Level2_Manager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Level2_Manager : MonoBehaviour
+{
+    #region Variables
+    public ObjectsReference objectsReference;
+
+    private bool showedTutorial = false;
+    #endregion
+
+    #region Main Methods
+    private void Start()
+    {
+
+    }
+    #endregion
+
+    #region Helper Method
+    public void ShowTutorial()
+    {
+        if (showedTutorial == false)
+        {
+            objectsReference.objectsInScene.TryGetValue("DialogOverlay", out GameObject dialog);
+            dialog.SetActive(true);
+            showedTutorial = true;
+        }
+        else
+        {
+            objectsReference.objectsInScene.TryGetValue("TutorialButton", out GameObject tutorialButton);
+            tutorialButton.SetActive(false);
+            objectsReference.objectsInScene.TryGetValue("DialogOverlay", out GameObject dialog);
+            dialog.SetActive(false);
+        }
+    }
+    public void GameStart()
+    {
+
+    }
+    public void GameReset()
+    {
+
+    }
+    #endregion
+}
---
+++ Assets/Scripts/ObjectsReference.cs
@@ -0,0 +1,17 @@
+﻿using RotaryHeart.Lib.SerializableDictionary;
+using PePo.Dev;
+using UnityEngine;
+
+public class ObjectsReference : MonoBehaviour
+{
+    public ObjectsInScene objectsInScene;
+}
+
+namespace PePo.Dev
+{
+    [System.Serializable]
+    public class ObjectsInScene : SerializableDictionaryBase<string, GameObject>
+    {
+
+    }
+}
