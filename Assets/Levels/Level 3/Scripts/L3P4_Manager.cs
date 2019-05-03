cbb5826b63501c945c3531eebe219307e1094717
PePo <gmgame.1525@gmail.com>
2019-05-03 23:12:39 +0700

    Add asset for Level 3  Page 4

---
+++ Assets/Levels/Level 3/Scripts/L3P4_Manager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L3P4_Manager : MonoBehaviour
+{
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+}
